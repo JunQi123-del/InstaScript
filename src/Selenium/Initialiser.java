7dacc001703c9359b1d646580ade794d26e84c84
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2023-10-18 23:57:26 +0800

    [Insta][Successfully opened chromedriver webpage]

--- src/Selenium/Initialiser.java
+++ src/Selenium/Initialiser.java
@@ -1,10 +1,18 @@
 package Selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Initialiser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();;
+		
+		driver.get("https://www.instagram.com/");
 	}
 
 }
