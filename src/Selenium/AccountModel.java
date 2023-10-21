1dd2469e9d06cb75158b3a67d967961ea21d809a
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2023-10-22 00:19:02 +0800

    [IS][created account model] JunQi 20231022

---
+++ src/Selenium/AccountModel.java
@@ -0,0 +1,21 @@
+package Selenium;
+
+public class AccountModel {
+
+	private String UserName;
+	private String password;
+	
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
